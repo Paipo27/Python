2fc783398663512c94e3ccddf3ce2f3f7aac65c5
Daniel <dcarrenoc@unal.edu.co>
2023-11-27 16:09:33 -0500

    correcion del codigo de distancia de hamming

--- Distancia de Hamming.py
+++ Distancia de Hamming.py
@@ -1,13 +1,12 @@
 def distanciaHA(str1, str2):
-    # Aseguramos que las cadenas tengan la misma longitud
-    if len(str1) != len(str2):
-        raise ValueError("Las cadenas deben tener la misma longitud para la distancia de Hamming.")
+      #se mira las cadenas y se busca la diferencia entre ellas para retornar el valor de la distancia de hamming
+      return sum(c1 != c2 for c1, c2 in zip(str1, str2))
 
-    # Contamos la cantidad de caracteres diferentes en las mismas posiciones
-    sum(c1 != c2 for c1, c2 in zip(str1, str2))
-    return sum
-# Ejemplo de uso
-cadena1 = "casa"
+ 
+
+
+#Ejemplo de la distancia de Hamming
+cadena1 = "perrito"
 cadena2 = "calle"
-resultado_hamming = distanciaHA(cadena1, cadena2)
-print(f"La distancia de Hamming entre '{cadena1}' y '{cadena2}' es: {resultado_hamming}")
+Distancia = distanciaHA(cadena1, cadena2)
+print(f"La distancia de Hamming entre '{cadena1}' y '{cadena2}' es: {Distancia}")
