ed3d1be88d7506e53f7026d0c32390bb6dc2fdcd
Daniel <dcarrenoc@unal.edu.co>
2023-11-27 16:11:56 -0500

    Se areglo para que solo reciba cadenas iguales

--- Distancia de Hamming.py
+++ Distancia de Hamming.py
@@ -1,4 +1,8 @@
 def distanciaHA(str1, str2):
+      #Se verifica que las cadenas tengan la misma longitud
+      if len(str1) != len(str2):
+            print("Las cadenas deben tener la misma longitud")
+            return None
       #se mira las cadenas y se busca la diferencia entre ellas para retornar el valor de la distancia de hamming
       return sum(c1 != c2 for c1, c2 in zip(str1, str2))
 
@@ -9,4 +13,5 @@ def distanciaHA(str1, str2):
 cadena1 = "perrito"
 cadena2 = "calle"
 Distancia = distanciaHA(cadena1, cadena2)
-print(f"La distancia de Hamming entre '{cadena1}' y '{cadena2}' es: {Distancia}")
+if Distancia != None:  
+      print(f"La distancia de Hamming entre '{cadena1}' y '{cadena2}' es: {Distancia}")
